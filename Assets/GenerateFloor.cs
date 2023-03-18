37778982b15beac9d73ae1b355f5aa87ee403c20
Kilian d'Eternod <kilian.deternod@epfl.ch>
2023-03-19 00:28:37 +0100

    AI car + randon gen

--- Assets/GenerateFloor.cs
+++ Assets/GenerateFloor.cs
@@ -6,21 +6,55 @@ public class GenerateFloor : MonoBehaviour
 {
 
     public GameObject building;
+    public GameObject AICar;
     public GameObject player;
     public int densityBuildings;
+    public int amountCars;
 
     int MAX_WIDTH = 500;
     int MAX_HEIGHT = 500;
 
+    public GameObject[] buildings;
+
+    public GameObject[] AICars;
+
     // Start is called before the first frame update
     void Start()
     {
+        buildings = new GameObject[densityBuildings];
+
+        AICars = new GameObject[amountCars];
+
         for (int i=0;i<densityBuildings;i++) {
-            int x = Random.Range(-MAX_WIDTH/2,MAX_WIDTH/2);
-            int z = Random.Range(-MAX_HEIGHT/2,MAX_HEIGHT/2);
-            if (Vector3.Distance(new Vector3(x,0,z),player.transform.position) > 10f) {
-                Instantiate(building, new Vector3(x,gameObject.transform.position.y+building.transform.localScale.y/2,z), Quaternion.identity);
-            }
+            float x=0;
+            float z=0;
+            do {
+                x = Random.Range(-MAX_WIDTH/2,MAX_WIDTH/2);
+                z = Random.Range(-MAX_HEIGHT/2,MAX_HEIGHT/2);
+            } while(Vector3.Distance(new Vector3(x,0,z),player.transform.position) < 10f);
+            
+            buildings[i] = (GameObject)Instantiate(building, new Vector3(x,gameObject.transform.position.y+building.transform.localScale.y/2,z), Quaternion.identity);
+        }
+
+        for (int i=0;i<amountCars;i++) {
+            float x=0;
+            float z=0;
+            bool occupied = false;
+            do {
+                x = Random.Range(-MAX_WIDTH/2,MAX_WIDTH/2);
+                z = Random.Range(-MAX_HEIGHT/2,MAX_HEIGHT/2);
+
+                occupied = false;
+                for (int j=0;j<densityBuildings;j++) {
+                    if (Vector3.Distance(new Vector3(x,0,z),buildings[j].transform.position) < 10f) {
+                        occupied = true;
+                        break;
+                    }
+                }
+
+            } while(!occupied && Vector3.Distance(new Vector3(x,0,z),player.transform.position) < 10f);
+
+            AICars[i] = (GameObject)Instantiate(AICar, new Vector3(x,gameObject.transform.position.y+1f,z), Quaternion.identity);
         }
     }
 
